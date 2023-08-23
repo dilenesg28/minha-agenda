2f2717881712fb64e076824a86ad80fded39e77c
dilenesg28 <cdilenes28@gmail.com.br>
2023-08-22 22:01:17 -0300

    aula-22-08-23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <nav className="bg-slate-900 p-4">
         <h1 className="text-3xl font-bold">Minha agenda</h1>
+        <h2 className='text-3xl font-semibold #border-grid-'>Nova aba</h2>
       </nav>
 
     </>    
